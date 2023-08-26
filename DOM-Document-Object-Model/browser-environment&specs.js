fc2538d3714faf0faf50c5c8c757e0762bed3716
sardorReact <rovshonicon@gmail.com>
2023-08-26 16:21:38 +0500

    BROWSER

--- DOM-Document-Object-Model/browser-environment&specs.js
+++ DOM-Document-Object-Model/browser-environment&specs.js
@@ -64,3 +64,38 @@ setTimeout(() => {
 // Hujjat uchun uslub qoidalarini o'zgartirganda CSSOM DOM bilan birgalikda ishlatiladi. Amalda, CSSOM kamdan-kam talab qilinadi, chunki biz kamdan-kam hollarda JavaScript-dan CSS qoidalarini o'zgartirishimiz kerak (odatda biz CSS sinflarini qo'shamiz/o'chiramiz, ularning CSS qoidalarini o'zgartirmaymiz), lekin bu ham mumkin.
 
 // #BOM - Browser Object Model
+// Brauzer ob'ekt modeli (BOM) documentdan tashqari hamma narsa bilan ishlash uchun brauzer (xost muhiti) tomonidan taqdim etilgan qo'shimcha ob'ektlarni ifodalaydi.
+
+// Masalan; misol uchun:
+// Navigator ob'ekti brauzer va operatsion tizim haqida ma'lumot beradi . Ko'pgina xususiyatlar mavjud, ammo eng keng tarqalgan ikkitasi: navigator.userAgent- joriy brauzer haqida va navigator.platform- platforma haqida (Windows/Linux/Mac va boshqalarni farqlashga yordam beradi).
+// location ob'ekti bizga joriy URL manzilini o'qish imkonini beradi va brauzerni yangisiga yo'naltirishi mumkin.
+
+// Ob'ektdan qanday foydalanishimiz mumkin location:
+alert(location.href); // shows current URL
+if (confirm("Go to Wikipedia?")) {
+  location.href = "https://wikipedia.org"; // redirect the browser to another URL
+}
+
+// Funktsiyalar alert/confirm/promptham BOMning bir qismidir: ular documnet bilan bevosita bog'liq emas, lekin foydalanuvchi bilan muloqot qilish uchun sof brauzer usullarini ifodalaydi.
+
+// Texnik xususiyatlari
+// BOM umumiy HTML spetsifikatsiyasining bir qismidir .Ha, siz buni to'g'ri eshitdingiz. Https://html.spec.whatwg.org saytidagi HTML spetsifikatsiyasi nafaqat "HTML tili" (teglar, atributlar), balki bir qator ob'ektlar, usullar va brauzerga xos DOM kengaytmalarini ham qamrab oladi. Bu "keng ma'noda HTML". Bundan tashqari, ba'zi qismlarda https://spec.whatwg.org saytida ko'rsatilgan qo'shimcha xususiyatlar mavjud .
+
+// Xulosa:
+// Standartlar haqida gapiradigan bo'lsak, bizda:
+
+// DOM spetsifikatsiyasi
+// Hujjat tuzilishi, manipulyatsiya va hodisalarni tavsiflaydi, qarang: https://dom.spec.whatwg.org .
+// CSSOM spetsifikatsiyasi
+// Uslublar jadvallari va uslublar qoidalari, ular bilan manipulyatsiyalar va hujjatlar bilan bog'lanishi tasvirlangan, qarang: https://www.w3.org/TR/cssom-1/ .
+// HTML spetsifikatsiyasi
+// HTML tilini (masalan, teglar) va shuningdek, BOMni (brauzer ob'ekt modeli) tavsiflaydi - turli xil brauzer funktsiyalari: setTimeout, alert, locationva hokazo, qarang: https://html.spec.whatwg.org . U DOM spetsifikatsiyasini oladi va uni ko'plab qo'shimcha xususiyatlar va usullar bilan kengaytiradi.
+// Bundan tashqari, ba'zi sinflar https://spec.whatwg.org/ saytida alohida tasvirlangan .
+
+// Iltimos, ushbu havolalarga e'tibor bering, chunki o'rganish uchun juda ko'p narsa borki, hamma narsani qamrab olish va hammasini eslab qolish mumkin emas.
+
+// Xususiyat yoki usul haqida oʻqishni istasangiz, https://developer.mozilla.org/en-US/ manzilidagi Mozilla qoʻllanmasi ham yaxshi manbadir, ammo mos keladigan xususiyatlar yaxshiroq boʻlishi mumkin: u murakkabroq va o'qish uchun uzoqroq bo'ladi, lekin asosiy bilimlaringizni mustahkam va to'liq qiladi.
+
+// Biror narsani topish uchun odatda “WHATWG [term]” yoki “MDN [term]” internet qidiruvidan foydalanish qulay, masalan, https://google.com?q=whatwg+localstorage , https://google.com?q =mdn+lokal xotira .
+
+// Endi biz DOMni o'rganishga kirishamiz, chunki hujjat UIda markaziy rol o'ynaydi.
