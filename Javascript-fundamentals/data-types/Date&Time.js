7dab6ea114779c3e9ee92417d161f0b022dc50c3
Sardor Rajabov <sardor@pop-os.localdomain>
2023-08-29 14:44:22 +0500

    Destructuring

---
+++ Javascript-fundamentals/data-types/Date&Time.js
@@ -0,0 +1,12 @@
+// Sana va Vaqtlar
+let salaries = {
+  "John": 100,
+  "Pete": 300,
+  "Mary": 250,
+};
+
+function topSalary({first, second, last}) {
+  return Object.entries()
+}
+
+console.log(topSalary(salaries))
