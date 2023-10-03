1651d220dac903fa8f231ce98e25ba7c7116c6d6
udsardor <udsardor@gmail.com>
2023-10-03 23:29:01 +0500

    first commit for advanced javascript section - Recursion&Stack

---
+++ Javascript-fundamentals/advanced-section-of-js/recursion&stack.js
@@ -0,0 +1,6 @@
+// Recursion & Stack
+// Keling funksiyalarga qaytaylik va uni chuqurroq o'rganaylik
+// Birinchi mavzu RECURSION:
+
+// Rekursiya bu dasturlashdagi patternlardan (pattern - umumiy va ko'p uchraydigan muammolarga umumiy yechim) biri hisoblanadi,u ko'plab tabiiy ravishda bo'laklarga bo'linadigan topshiriqlar lekin, osonroq ko'rinadi. Yoki birxil bo'lgan topshiriqning osonroq yozilishi yoki ko'rinishi desak ham bo'ladi. Yoki yaqinda ko'radigan aniq bir ma'lumotlar strukturalari bilan ishlash uchun.
+
